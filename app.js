db6a09bd1d10b1b9c39d4c9d9e87525d84166d59
xdang <xdangleterre@hotmail.com>
2026-02-18 11:18:26 +0100

    Mise en place du premier app avec express

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 console.log("Hello Node . Comment ça va ? ")
+
+//on met en place le premier point de terminaison du projet:
+// Ce point de terminaison va renvoyer les résultats des premières requêtes .
+
+// On charge express : il va permettre de mettre en place notre api de base
+const express = require ('express');
+
+//On recrée l'app en utilisant le module express devenu une constante
+const app = express();
+
+//On définit le port sur lequel on effectue l'écoute 
+const port = 3000;
+
+// On met en place notre premier entry point : Une réponse à la requête
+// Il utilise une fonction javascript avec un endpoit / qui est la racine 
+// et une fonction fléchée  req , res , qui va recevoir la requête et renvoyer la réponse res 
+app.get("/", (req,res) => res.send(`Hello , express! yayyyy !`))
+
+//On lance l'écoute sur le port que l'on a définit (ie => 3000)
+app.listen(port, () => console.log(`Notre application Node est démarré sur : http:// localhost:${port}. Youpiiii ! ` ))
